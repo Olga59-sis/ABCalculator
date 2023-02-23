db895408bbb1ef7c83f7e0fed9a1e54f8275e6eb
Olga59-sis <aglokazachinskaya@gmail.com>
2023-02-23 23:00:09 +0300

    Cчитывание данных из полей ввода

--- ABCalculator.py
+++ ABCalculator.py
@@ -5,8 +5,18 @@ import tkinter as tk
 # Функция закрытия программы
 def do_close():
     root.destroy()
-
-def popup_window():
+    
+def do_processing():
+    #Считывание данных из полей ввода
+    n1 = int(entVisitors1.get())
+    c1 = int(entConversions1.get())
+    n2 = int(entVisitors2.get())
+    c2 = int(entConversions2.get())
+    
+    popup_window(n1, c1, n2, c2)
+    
+#Функция вызова окна результатов
+def popup_window(n1, c1, n2, c2):
     window = tk.Toplevel()
     window.geometry("280x300")
     window.title("A/B результат")
@@ -15,8 +25,10 @@ def popup_window():
     btnClosePopup = tk.Button(window, text="Закрыть", font = ('Helvetika', 10, 'bold'), command=window.destroy )
     btnClosePopup.place(x=160, y=250, width=90, height=30)
 
-# Создание главного окна
+    #Перевод фокуса на созданное окно
+    window.focus_force()
 
+# Создание главного окна
 root = tk.Tk()
 root.geometry("280x300")
 root.title("A/B калькулятор")
@@ -64,7 +76,7 @@ entConversions2.place(x=115,y=205, width=90, height=20)
 entConversions2.insert(tk.END,'0')
 
 # Добавление кнопки "Рассчитать"
-btnProcess = tk.Button(root, text="Рассчитать", font = ('Helvetika', 10, 'bold'), command=popup_window)
+btnProcess = tk.Button(root, text="Рассчитать", font = ('Helvetika', 10, 'bold'), command=do_processing)
 btnProcess.place(x=25, y=250, width=90, height=30)
 
 # Добавление кнопки закрытия программы
